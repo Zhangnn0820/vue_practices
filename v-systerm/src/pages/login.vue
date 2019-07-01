aa74654edc5a8ca877e47d0b0555afff649d615d
Pumpkinznn <nannancassiopeia@126.com>
2019-07-01 21:58:33 +0800

    vue systerm'

---
+++ v-systerm/src/pages/login.vue
@@ -0,0 +1,85 @@
+<template>
+    <div class="container">
+        <el-form :model="ruleForm" status-icon :rules="rules" ref="ruleForm" label-width="100px" class="demo-ruleForm">
+    <el-form-item label="用户名" prop="username">
+      <el-input type="text" v-model="ruleForm.username" autocomplete="off"></el-input>
+    </el-form-item>
+    <el-form-item label="密码" prop="password">
+      <el-input type="password" v-model="ruleForm.password" autocomplete="off"></el-input>
+    </el-form-item>
+    
+    <el-form-item>
+      <el-button type="primary" @click="submitForm('ruleForm')">提交</el-button>
+      <el-button @click="resetForm('ruleForm')">重置</el-button>
+    </el-form-item>
+  </el-form>
+
+    </div>
+ 
+</template>
+
+<script>
+  export default {
+    data() {
+      var validatePass = (rule, value, callback) => {
+        if (value === '') {
+          callback(new Error('请输入用户名'));
+        } else {
+          if (this.ruleForm.password !== '') {
+            this.$refs.ruleForm.validateField('checkPass');
+          }
+          callback();
+        }
+      };
+      var validatePass2 = (rule, value, callback) => {
+        if (value === '') {
+          callback(new Error('请输入密码'));
+        } else {
+          callback();
+        }
+      };
+      return {
+        ruleForm: {
+          username: '',
+          password: '',
+        },
+        rules: {
+          username: [
+            { validator: validatePass, trigger: 'blur' }
+          ],
+          password: [
+            { validator: validatePass2, trigger: 'blur' }
+          ],
+        }
+      };
+    },
+    methods: {
+      submitForm(formName) {
+        this.$refs[formName].validate((valid) => {
+          if (valid) {
+            alert(this.ruleForm.password)
+            alert('submit!');
+          } else {
+            console.log('error submit!!');
+            return false;
+          }
+        });
+      },
+      resetForm(formName) {
+        this.$refs[formName].resetFields();
+      }
+    }
+  }
+</script>
+
+
+<style>
+  body{
+    background-color: #555;
+  }
+  .demo-ruleForm{
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+  }
+</style>
---
+++ v-systerm/src/router/index.js
@@ -0,0 +1,17 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+
+
+import login from '@/pages/login'
+Vue.use(Router)
+
+
+export default new Router({
+  routes: [
+    {
+      path: '/',
+      name: 'login',
+      component: login
+    }
+  ]
+})
